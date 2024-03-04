2706b8b3c0139c0095244ec63930c61001145cc4
Laraib Rizwan <161979255+Laraib2005@users.noreply.github.com>
2024-03-04 06:26:15 -0800

    Add files via upload
    typescript chapter :variables slide home task #governorsindh#itcourse#artifficialintelligence

---
+++ main.ts
@@ -0,0 +1,46 @@
+console.log("hello world");
+console.log("message");
+//ali plz dont message
+
+let age:number=21;
+let num1="1"
+let num2="1"
+console.log(num1+num2);
+let num3=1;
+let num4="1";
+console.log(num3+num4);
+let num5=1;
+let num6=2;
+console.log(num5+num6);
+let str1 ="1";
+let str2 ="2";
+console.log(str1+str2);
+let num7=5;
+let num8=2;
+console.log(num7-num8);
+let num9=5;
+let num10=2;
+console.log(num9*num10);
+let num11=4;
+let num12=2;
+console.log(num11/num12);
+let num13=2;
+let num14=2;
+console.log(num13**num14);
+let num15=10;
+let num16=3;
+console.log(num15%num16);
+let n=5;
+console.log(n);
+n+=5;
+console.log(n);
+n-=5;
+console.log(n);
+let n18= 5;
+console.log(n18==5);
+console.log(n18===5);
+console.log(n18!=5);
+console.log(n18>8);
+console.log(n18<8);
+console.log(n18>=8);
+console.log(n18<=8);
